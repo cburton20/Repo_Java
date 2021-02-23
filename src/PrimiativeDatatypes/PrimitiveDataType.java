d9bac015d9e57bc8d04b2944bb709f53bec9fc77
Carlton Burton <carltonburton@Carltons-MacBook-Pro.local>
2021-02-23 10:53:14 -0500

    First homework

---
+++ src/PrimiativeDatatypes/PrimitiveDataType.java
@@ -0,0 +1,37 @@
+package PrimiativeDatatypes;
+
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+
+        byte numOfHoursInADay = 24;
+        System.out.println("Number of hours in a day :" + numOfHoursInADay);
+
+        short maxNumOfDaysInAYear = 365;
+        System.out.println("Max number of days in a year :" + maxNumOfDaysInAYear);
+
+        int numOfEmployeesAtAmazon = 575700;
+        System.out.println("Total number of employees at Amazon is :" + numOfEmployeesAtAmazon );
+
+        long totalPopulationOfUsa = 331000000L;
+        System.out.println("Total population of USA in 2021 is :" + totalPopulationOfUsa);
+
+        float currentMortgageRate = 2.5F;
+        System.out.println("The current mortgage rate is :" + currentMortgageRate);
+
+        double myCurrentAccountBalance = 50000.75;
+        System.out.println("My current account balance is :" + myCurrentAccountBalance);
+
+        boolean sunRiseOnTheWest = false;
+        System.out.println("Does the sun rise start in the west? " + sunRiseOnTheWest);
+
+        char firstInitial = 'C';
+        System.out.println("My first initial is :" + firstInitial);
+
+        char lastInitial = 'B';
+        System.out.println("My last initial is :" + lastInitial);
+
+
+
+
+    }
+}
